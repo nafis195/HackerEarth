48d956eb5507c3c34247d5b1d988fde7699b97de
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2019-12-27 18:53:28 -0800

    Add files via upload

--- Simple Search/Source.cpp
+++ Simple Search/Source.cpp
@@ -1,1 +1,33 @@
+// Bismillahir Rahmanir Rahim
+// Nafis Chowdhury
+// HackerEarth - Simple Search
+// Problem Statement - https://www.hackerearth.com/practice/algorithms/searching/linear-search/practice-problems/algorithm/simple-search-4/
+// Main.cpp
 
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	int n;
+	long long int k;
+	cin >> n;
+	long long int a[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+
+	cin >> k;
+
+	for (int i = 0; i < n; i++)
+	{
+		if (a[i] == k)
+			cout << i << endl;
+	}
+
+	return 0;
+}
